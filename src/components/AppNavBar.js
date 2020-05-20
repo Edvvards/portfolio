6eed6aa1fb4f8867f616496197187c8df4e90a94
liamedwards2010@gmail.com <liamedwards2010@gmail.com>
2020-05-20 11:40:06 +0100

    fixed nav blur

--- src/components/AppNavBar.js
+++ src/components/AppNavBar.js
@@ -48,7 +48,9 @@ class AppNavBar extends Component {
               </nav>
     }
     return (
-        <header>
+        <header 
+        className={this.state.isActive ? 'remove-blur': null}
+        >
           <p className="logo">Liam Edwards</p>
             { menu }
         </header>
@@ -57,3 +59,40 @@ class AppNavBar extends Component {
 }
 
 export default AppNavBar;
+
+
+class MyComponent extends Component {
+  constructor(props) {
+      super(props);
+      this.addActiveClass= this.addActiveClass.bind(this);
+      this.state = {
+          active: false,
+      };
+  }
+  toggleClass() {
+      const currentState = this.state.active;
+      this.setState({ active: !currentState });
+  };
+
+  render() {
+      return (
+          <div 
+              className={this.state.active ? 'your_className': null} 
+              onClick={this.toggleClass} 
+          >
+              <p>{this.props.text}</p>
+          </div>
+      )
+}
+}
+
+class Test extends Component {
+  render() {
+      return (
+          <div>
+              <MyComponent text={'1'} />
+              <MyComponent text={'2'} />
+          </div>
+      );
+  }
+}
